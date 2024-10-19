ac440d87d444c1112a723e00f0e70db4a8fe3e53
Злата Маскалевич <msk.zl.06.26@gmail.com>
2024-10-19 03:19:55 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
 
+const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
+
 // @todo: Функция создания карточки
+function createCard(data, handleDelete) {
+
+    const cardElement = cardTemplate.cloneNode(true);
+    
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    cardTitle.textContent = data.name;
+
+    deleteButton.addEventListener('click', handleDelete);
+
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
 
+function handleDelete(event) {
+    const cardElement = event.target.closest('.card');
+    cardElement.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData, handleDelete);
+    cardsContainer.append(cardElement);
+});
